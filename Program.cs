5d4f0288dec035d8dda021733b8fb579c151829a
kissd95 <oldtrue69@gmail.com>
2020-10-03 09:57:40 +0200

    Beolvasas, bekeres,kiiras,

--- Program.cs
+++ Program.cs
@@ -9,12 +9,8 @@ namespace lotto1
 {
     class Program
     {
-        static int szam;
-        static void Main(string[] args)
+        static  void Beolvasas()
         {
-            int[] szamok = new int[5];
-            int szam;
-
             string fajl = "otos.csv";
             StreamReader sr = null;
             using (sr = new StreamReader(fajl))
@@ -22,34 +18,63 @@ namespace lotto1
                 sr.ReadLine();
                 while (!sr.EndOfStream)
                 {
-                    
+                    string sor = sr.ReadLine();
+                    string[] temp = sor.Split(';');
+
+
 
                 }
 
-                for (int i = 0; i < szamok.Length; i++)
+            }
+        }
+        
+        static void Bekeres()
+        {
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.WriteLine("Kérek egy számot! ");
+                szam = int.Parse(Console.ReadLine());
+                if (szam > 0 && szam <= 90)
                 {
-                    Console.WriteLine("Kérek egy számot! ");
+                    szamok[i] = szam;
+                }
+                else
+                {
+                    Console.WriteLine("0-és 90 között kell bekérni! Adj meg másik számot! ");
                     szam = int.Parse(Console.ReadLine());
-                    if (szam > 0 && szam <= 90)
-                    {
-                        szamok[i] = szam;
-                    }
-                    else
-                    {
-                        Console.WriteLine("0-és 90 között kell bekérni! Adj meg másik számot! ");
-                        szam = int.Parse(Console.ReadLine());
-                        szamok[i] = szam;
-                    }
+                    szamok[i] = szam;
                 }
+            }
+        }
 
-                for (int i = 0; i < szamok.Length; i++)
-                {
-                    Console.WriteLine(szamok[i]);
-                }
+        static void Kiiras()
+        {
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.WriteLine(szamok[i]);
+            }
+        }
+        
+
+        static int szam;
+        static int[] szamok = new int[5];
+        static void Main(string[] args)
+        {
+            Beolvasas();
+            Bekeres();
+            Kiiras();
+            
+
+           
+                
+
+               
+
 
 
                 Console.ReadKey();
             }
         }
     }
-}
+
